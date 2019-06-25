02cfa8a688ce97107a06e30f93eb9c9820bbb293
Paul Duffell <duffell@Pauls-MacBook-Pro-5.local>
2019-06-25 16:17:33 -0400

    Made Backgnd Disk Parameterizable

--- gap.py
+++ gap.py
@@ -30,12 +30,12 @@ def one_planet( r = 1.0 , rp = 1.0 , q = 1e-3 , alpha = 0.01 , Mach = 20. ):
 
 #The Background Density Profile:
 
-def sigma_backgnd( r = 1.0 ):
+def sigma_backgnd( r , disk_params ):
 	S = math.exp( -(r/80.)**1.5 )*math.exp( -(r/110.)**10. )
 	return S
 
-def get_sigma( r , rs , qs , alphas , Machs ):
-        Sigma = sigma_backgnd(r)
+def get_sigma( r , rs , qs , alphas , Machs , disk_params ):
+        Sigma = sigma_backgnd( r , disk_params )
 	Np = len(rs)
 	for j in range(Np):
 		Sigma *= one_planet( r , rs[j] , qs[j] , alphas[j] , Machs[j] )
@@ -48,6 +48,10 @@ rs = [13.1,33.0,68.6]
 alphas = [1e-3,1e-3,1e-3]
 Machs = [10.,12.,14.]
 
+#Potential Disk Parameters to Feed to sigma_backgnd:
+
+disk_params = [1,2,3,4,5,6]
+
 #Range and Resolution of Output:
 
 Rmin = 0.1
@@ -58,8 +62,8 @@ Npts = 1000
 for i in range(Npts):
 	x = (i+.5)/Npts
 	r = Rmin*(Rmax/Rmin)**x
-	S_bkgnd = sigma_backgnd(r)
-	Sigma = get_sigma( r , rs , qs , alphas , Machs )
+	S_bkgnd = sigma_backgnd( r , disk_params )
+	Sigma = get_sigma( r , rs , qs , alphas , Machs , disk_params )
 	print r , Sigma , S_bkgnd
 
 	
